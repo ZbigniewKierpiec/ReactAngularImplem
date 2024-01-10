2288484f57a5885bb4c4a218da77f0d019a991fe
zee-creato <zbigniew.kierpiec@proav.com>
2024-01-10 21:09:55 +0000

    xxx

--- src/Components/Zaluzje/Zaluzje.jsx
+++ src/Components/Zaluzje/Zaluzje.jsx
@@ -12,9 +12,77 @@ export default function Zaluzje(props) {
   const [number, setNumber] = useState(100);
   const [active, setActive] = useState(false);
   const [active2, setActive2] = useState(false);
+ 
+  // const [activeText, setActiveText] = useState(false);
   const app = useRef(null);
+       
+  
+
+
+
+
+
+  // useEffect(() => {
+  //   const getBoundingClientRect = () => {
+  //     const element = app.current;
+  //     if (element) {
+  //       const rect = element.getBoundingClientRect();
+  //       console.log('Element position:', rect.y);
+  //     }
+  //   };
+
+  //   // Call the function on mount or whenever the component updates
+  //   getBoundingClientRect();
+
+  //   // Optionally, you can add an event listener to recalculate on resize or other events
+  //   window.addEventListener('resize', getBoundingClientRect);
+
+  //   // Cleanup the event listener on component unmount
+  //   return () => {
+  //     window.removeEventListener('resize', getBoundingClientRect);
+  //   };
+  // }, []); // Empty dependency array ensures the effect runs only once on mount
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   useEffect(() => {
+
     gsap.registerPlugin(Draggable);
     let startX, startY;
     const draggableInstance = Draggable.create(app.current, {
@@ -30,20 +98,65 @@ export default function Zaluzje(props) {
       onDragEnd: function () {
         // Handle drag end event
         setActive(false);
-        setTimeout(()=>{
-           setActive2(false)
-        },2000)
+
+        setTimeout(() => {
+          setActive2(false);
+        }, 2000);
       },
     });
-
+    
     function updateDirection() {
+
+
+
+
+
+
+
+
+      const getBoundingClientRect = () => {
+        const element = app.current;
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          console.log('Element position:', rect );
+        }
+      };
+  
+      // Call the function on mount or whenever the component updates
+      getBoundingClientRect();
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       const xChange = this.x - startX;
       const yChange = this.y - startY;
       const ratio = Math.abs(xChange / yChange);
       const newDirection = [];
       if (newDirection) {
         setActive(true);
-        setActive2(true)
+        setActive2(true);
       }
       if (ratio > 0.25) {
         newDirection.push(xChange < 0 ? "left" : "right");
@@ -57,6 +170,7 @@ export default function Zaluzje(props) {
 
       const yPosition = this.y;
       const containerHeight = app.current.clientHeight;
+
       const percentage = Math.round(
         (Math.abs(yPosition) / containerHeight) * 100
       );
@@ -83,7 +197,9 @@ export default function Zaluzje(props) {
       <div className={`${styles.mask}  `}>
         <div ref={app} className={styles.blind}>
           <div className={styles.info}>
-            <span className={`${styles.text}  ${active2 ? styles.active : ""}`}>
+            <span
+              className={`${styles.text}   ${active2 ? styles.active : ""}  `}
+            >
               {number}
             </span>
 
@@ -97,7 +213,9 @@ export default function Zaluzje(props) {
           </div>
         </div>
 
-        <div className={styles.blindBackground}></div>
+        <div className={styles.blindBackground}>
+          <span className={`${styles.spanText}`}>{number}</span>
+        </div>
       </div>
     </div>
   );
