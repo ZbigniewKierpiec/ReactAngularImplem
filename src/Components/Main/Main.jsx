e137be81d9e4be9198fd854056dfab32f1218ff8
zee-creato <zbigniew.kierpiec@proav.com>
2024-01-12 13:20:19 +0000

    xxx

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -44,7 +44,7 @@ const reducer = (state, action) => {
 
     default:
       // If no specific case matches, return the default home component
-      return { component: <Power/> };
+      return { component: <Matrix/> };
   }
 };
 export default function Main({ active }) {
